2052ce5bc67abeab744acd9afb2497010a09b6f0
Jon L <blinkcloudinfo@gmail.com>
2020-04-29 12:59:53 -0400

    updating base server modules

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 var mongoose = require('mongoose');
 var unirest = require("unirest");
 
+//Local Env Variables
+const dotenv = require('dotenv');
+    dotenv.config();
+    console.log('Currently deployed on ${process.env.NODE_ENV' );
+
+
 var req = unirest("GET", "https://www.tsawaittimes.com/api/airport/7pA4uDewjNXMAx1qhzhaxGFjVHcmT9qV/ATL/JSON");
 
 req.end(function (res) {
