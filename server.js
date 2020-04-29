2003037ec561451d93ddc30914eaecd795e45e8a
Jon L <blinkcloudinfo@gmail.com>
2020-04-29 14:38:52 -0400

    resolved port issues

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var unirest = require("unirest");
-
-//Local Env Variables
-const dotenv = require('dotenv');
-    dotenv.config();
-    console.log('Currently deployed on ${process.env.NODE_ENV' );
+const express = require('express')
+const app = express()
+const unirest = require('unirest')
+const port = process.env.TSA_ENV_PORT
 
 
-var req = unirest("GET", "https://www.tsawaittimes.com/api/airport/7pA4uDewjNXMAx1qhzhaxGFjVHcmT9qV/ATL/JSON");
 
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
+//Routes
+app.get('/', (req, res) => {
+    res.status(200).json({
+        currentTime: Date.now(),
+        userMessage: "request success."
+    })
+})
+app.get('/api/v1/test', (req, res) => {
+    res.status(200).json({
+        currentTime: Date.now(),
+        userMessage: "request success."
+    })
+})
 
-	console.log(res.body);
-});
+// var req = unirest("GET", "https://www.tsawaittimes.com/api/airport/7pA4uDewjNXMAx1qhzhaxGFjVHcmT9qV/ATL/JSON");
+
+// req.end(function (res) {
+// 	if (res.error) throw new Error(res.error);
+
+// 	console.log(res.body);
+// });
+
+
+app.listen(port, () => 
+    console.log(`Express Server started on ${process.env.NODE_ENV} port ${port}, press ctrl-C to terminate.`)
+)
