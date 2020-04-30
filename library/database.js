96b5d6db2baf26e77a20d38503934b5a040b271b
jzade <62847140+jzade@users.noreply.github.com>
2020-04-30 02:07:25 -0400

    update/bug fix/added pouchDB - successfully loading airports-meta.json file into pouchDB - next is laying on full-test index library txi to support autocomplete feature/fuzzy search.  Also, methods for geohashing need to be built out

--- library/database.js
+++ library/database.js
@@ -1,7 +1,7 @@
-import Airport from '..model/airport.js'
-import { publicDecrypt } from 'crypto';
+// import Airport from '..model/airport.js'
+// import { publicDecrypt } from 'crypto';
 
-function AirportDB(sourcefilepath){
+exports.AirportDB = (sourcefilepath) => {
 
     //INNER VARIABLES
     var that = this;
@@ -12,51 +12,60 @@ function AirportDB(sourcefilepath){
     //INITIALIZATION FUNCTION
     var init = function(){
 
-        ///INIT THE POUCH DATABASE
-        var PouchDB = require('pouchdb');
-        that.db = new PouchDB('airport_database');
+        ///INIT THE POUCH DATABASE - jank code :P
+        var PouchDB = require('pouchdb')
+        that.db = new PouchDB('airport_database')
+
+        that.db.destroy()
+        
+        that.db = new PouchDB('airport_database')
 
         //INIT THE FULL TEXT
-        var TXI = require('txi');
-        that.textIndex = Txi();
+        const txi = require('txi')
+        
+        that.textIndex = txi()
 
-
-        var fs = require('fs');
-        var path = require('path');
+        var fs = require('fs')
+        var path = require('path')
       
         // Load Airport Data
-        var BUFFER = bufferFile(sourcefile);
+        var BUFFER = bufferFile(that.sourcefilepath)
       
         function bufferFile(relPath) {
-            return fs.readFileSync(path.join(__dirname, relPath)); 
+            return fs.readFileSync((relPath));
           }
 
-        var AirportsDecodedObj = JSON.parse(BUFFER.text);
-        var AirDataArray = AirportsDecoded.responsePayload;       
+        var AirportsDecodedObj = JSON.parse(BUFFER)
+        var AirDataArray = AirportsDecodedObj.responsePayload;       
         
         //IF IS NOT NULL AND IS ARRAY --TODO
         if(AirDataArray != null)
-            AirDataArray.forEach(loadAirport);
+            AirDataArray.forEach(loadAirport)
 
         function loadAirport(airport, index) {
-            await that.db.put(airport);
-            await that.textIndex.index(airport.code, airport);
+     
+            airport._id = airport.code + index
+          //  console.log(airport)
+           // console.log(that.db)
+            that.db.put(airport).then()
+            .catch(err => console.log(err))
+             that.textIndex.index(airport.code, airport).catch(err => console.log(err))
         }
-    };
+    }
 
     //FIND BY LATITUDE
     var findAirportByLatLong = function(latitude, longitude){
         return null;
-    };
+    }
 
     //FIND BY 
     var findAirportByText = function(text){
         return null;
-    };
+    }
 
     var findAirportByCode= function(code){
         return null;
-    };
+    }
 
 
     //STARTUP
